5bd91b436f6bec485369bbd657efd9f02f004eaa
davenportnw <davenportnw@gmail.com>
2020-07-16 08:43:49 -0500

    Fixing the routing

--- server.js
+++ server.js
@@ -1,41 +1,49 @@
-var express = require ('express');
+const express = require ('express');
 const app = express();
 const fs = require('fs');
+const path = require('path');
 
-const notes = '/Users/nicoledavenport/utcode/note_taker/Develop/public/notes.html';
-const index = '/Users/nicoledavenport/utcode/note_taker/Develop/public/index.html';
-const db = '/Users/nicoledavenport/utcode/note_taker/Develop/db/db.json';
-app.use(express.json());
 
 //Create Server 
-var server = app.listen(3000);
+var server = app.listen(3030);
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "Develop/public")));
+
+/* HTML Routing */
 //Homepage route
-app.get('/', function(req, res) {
-    res.sendFile(index)
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
 });
 
 //Notes route
 app.get('/notes.html', function(req,res) {
-    res.sendFile(notes)
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
 });
 
-//API - returned notes
+
+/* API Routes */
 
 app.get('/api/notes', function(req, res) {
-   const savedNotes =  fs.readFile(db , (err, data) => {
-       if(err) throw err;
-   })
-    res.sendFile(savedNotes)
-});
+    res.sendFile(path.join(__dirname,'Develop/db/db.json'));
+})
+
+
+// app.get('/api/notes', function(req, res) {
+//    const savedNotes =  fs.readFile(db , (err, data) => {
+//        if(err) throw err;
+//    })
+//     res.sendFile(savedNotes)
+// });
 
 //Saved notes
 
-app.post('/api/notes/:id', function (req, res) {
-    console.log(req.body);
-    const addedNotes = res.send(db);
-    const addAll = fs.appendFile(addedNotes, (err, data) => {
-        if(err) throw err;
-    })
-    res.sendFile(addAll);
-});
+// app.post('/api/notes/:id', function (req, res) {
+//     console.log(req.body);
+//     const addedNotes = res.send(db);
+//     const addAll = fs.appendFile(addedNotes, (err, data) => {
+//         if(err) throw err;
+//     })
+//     res.sendFile(addAll);
+// });

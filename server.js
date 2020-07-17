c91ce7402ec204aec87ae432a6f859d3b7cb13f0
davenportnw <davenportnw@gmail.com>
2020-07-17 09:36:33 -0500

    Test file.

--- server.js
+++ server.js
@@ -2,23 +2,27 @@ const express = require ('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-
-
-//Create Server 
-var server = app.listen(3030);
-//Heroku Server
-var PORT = process.env.PORT || 3030;
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT)
-});
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, "Develop/public")));
+const { get } = require('http');
 
 //Note Array 
 
 let notes = [];
+
+//Create Server 
+// var server = app.listen(3060);
+//Heroku Server
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT)
+});
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "Develop/public")));
+
+
 /* HTML Routing */
 
 //Homepage route
@@ -29,22 +33,36 @@ app.get('/', function(req, res) {
 //Notes route
 app.get('/notes', function(req,res) {
     res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+    
+});
+
+app.get('/api/notes', function(res, req) {
+    res.sendFile(path.json(_dirname, "Develop/db/db.json" ));
+});
+
+app.get("*", function(req, res) {
+    res.sentFile(path.join(_dirname, "Develop/public/index.html"));
 });
 
 
 /* API Routes */
 
-//Return saved files
+//Return SAVED files
 
 app.get('/api/notes', function(req, res) {
-    res.sendFile(path.join(__dirname,'Develop/db/db.json'));
-})
+    notes = fs.readFile(path.join('Develop/db/db.json'));
+    notes = JSON.parse(notes);
+    res.json(notes);
+});
 
-//Recieve new note - save on the reqest body -add to db.json
+//Recieve NEW note - SAVE on the reqest body -ADD to db.json
 
 app.post('/api/notes', function(req, res){ 
     //read past note(s)
-    notes = fs.readFile('Develop/db/db.json', 'utf8');
+  notes = fs.readFile('Develop/db/db.json', 'utf8', function(err) {
+        if (err) throw err;
+    });
+    console.log(notes);
     //parse the note
     notes = JSON.parse(notes);
     //add it to the body
@@ -57,7 +75,7 @@ app.post('/api/notes', function(req, res){
     });
     res.json(JSON.parse(notes));
 });
-//Delete note
+//DELETE note
 
 app.delete('/api/notes/:id', notes, function(err){
     if (err) throw err;
@@ -81,5 +99,3 @@ app.delete('/api/notes/:id', notes, function(err){
     res.send(JSON.parse(notes));
 
 });
-
-//  

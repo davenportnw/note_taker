9239812852515f7132c11b7fec44289f74fd423e
davenportnw <davenportnw@gmail.com>
2020-07-20 18:37:25 -0500

    Updating syntax

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
 const { get } = require('http');
-let db = path.resolve("Develop/db/db.json");
-// app = express.Router();
+const { join } = require('path');
 
-//Note Array 
 
-let notes = [];
-//Create Server 
-// var server = app.listen(3060);
+
+
 //Heroku Server
-var PORT = process.env.PORT || 3030;
+var PORT = process.env.PORT || 3060;
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT)
@@ -22,98 +19,101 @@ app.listen(PORT, function() {
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("Develop/public"));
+app.engine('html', require('ejs').renderFile);
 
 
+let idNumber = 1;
 /* HTML Routing */
 
 //Homepage route
+var rootPage = {root: __dirname + '/Develop/public'};
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+    console.log("test");
+    res.sendFile('/index.html', rootPage);
 });
 
 //Notes route
+
 app.get('/notes', function(req,res) {
-    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
-
-});
-app.get('/notes.html', function(res, req) {
-    res.sendFile(path.json(_dirname, "Develop/db/db.json" ));
+    res.sendFile('/notes.html', rootPage);
 });
 
-app.get("*", function(req, res) {
-    res.sentFile(path.join(_dirname, "Develop/public/index.html"));
-});
+// app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+// });
 
 
 /* API Routes */
 
 //Return SAVED files
 
-app.get('/notes.html', function(req, res) {
-  fs.readFile(path.resolve(db, "db.json"), "utf8", (err, data) =>{;
-    if (err) throw err;
-    res.json(data);
-    })
+app.get('/api/notes', function(req, res, next) {
+    let json = getJson();
+    console.log(json);
+    res.json(json);
+    next()
   });
 
 //Recieve NEW note - SAVE on the reqest body -ADD to db.json
 
-app.post('/notes.html', function(req, res){ 
-    //read past note(s)
-    let notes = fs.readFileSync(path.resolve(db, "db.json"), "utf8", function(err) {
-        if (err) throw err;
-    });
-    console.log(notes);
-    //parse the note
-    let notesArr = JSON.parse(notes);
-
-    //saving user input
-    const userInput = {
-        id: idNumber,
-        title: req.body.tile,
-        text: req.body.text
-    }
-    
-    idNumber += 1;
-    //add it to the body
-    console.log(userInput);
-    //stringify input
-
-    const userInputString = JSON.stringify(userInput);
-    //adding new notes
-    userInputString.push(req.body);
-
-    fs.writeFileSync(path.resolve(db, "db.json"), "utf8", function(err) {
-        if (err) throw err;
-    });
-
-    res.json(userInputString);
-
-    
-  
+app.post('/api/notes', function(req, res, next) {
+    console.log('api POST');
+    addNotetoJson(req.body);
+    res.json(getJson());
+    next()
    
 });
-//DELETE note
 
-app.delete('/notes.html/:id', notes, function(err){
-    if (err) throw err;
+//DELETE selected notes
 
-    notes = fs.readFile("Develop/db/db.json", notes, "utf8", function(err) {
-        if (err) throw err;
-    });
-
-    notes = JSON.parse(notes);
-
-    notes = notes.filter(function(note) {
-        return note.id != req.params.id;
-    });
-
-    notes = JSON.stringify(notes);
-
-    fs.writeFile("Develop/db/db.json", "utf8", function(err) {
-        if (err) throw err;
-    });
-
-    res.send(JSON.parse(notes));
-
+app.delete('/api/notes/:id', function(req, res, next) {
+    deleteNote(req.params.id);
+    // console.log("req.params.id", req.params.id);
+    res.json(getJson());
+    console.log("req.params.id", req.params.id);
 });
+
+
+
+//Functions to add/delete/save
+
+//Get the json
+function getJson() {
+    let data = fs.readFileSync(__dirname + "/Develop/db/db.json");
+    let json = JSON.parse(data);
+    return json;
+}
+// return json as an object
+
+function noteObject(data) {
+    idNumber += 1;
+    let userInput = {
+        id: idNumber,
+        title: data.text,
+        text: data.text,
+        complete: false,
+        hidden: false
+    }
+
+    return userInput;
+
+}
+
+function addNotetoJson(note) {
+    let json = getJson();
+    let newNote = noteObject(note);
+    json.push(newNote);
+    // console.log("new json", json);
+    saveNote(json);
+}
+function saveNote(jsonData) {
+    let data = JSON.stringify(jsonData);
+    fs.writeFileSync(__dirname + '/Develop/db/db.json', data);
+} 
+
+function deleteNote(id) {
+    let json = getJson();
+    json[id].hidden = true;
+    saveJson(json);
+}
